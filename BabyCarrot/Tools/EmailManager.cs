976f2b8e1509895d9815270440712822b66cb718
kiuk <kiuk@LAPTOP-9FDUHI6E>
2022-05-03 19:21:54 -0300

    초기 커밋.

---
+++ BabyCarrot/Tools/EmailManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace BabyCarrot.Tools
+{
+    public class EmailManager
+    {
+        public static void Send(string to, string subject, string contents)
+        {
+            string sender = "do_not_reply@test.com";
+
+            string smtpHost = "smtp.com";
+            int smtpPort = 2525;
+
+            string smtpId = "id";
+            string smtpPwd = "password";
+
+
+            MailMessage mailMsg = new MailMessage();
+            mailMsg.From = new MailAddress(sender);
+            mailMsg.To.Add(new MailAddress(to));
+
+            mailMsg.Subject = subject;
+            mailMsg.IsBodyHtml = true;
+            mailMsg.Body = contents;
+            mailMsg.Priority = MailPriority.Normal;
+        
+            SmtpClient smtpClient  = new SmtpClient();
+            smtpClient.Credentials = new NetworkCredential(smtpId, smtpPwd);
+            smtpClient.Host = smtpHost;
+            smtpClient.Port = smtpPort;
+            smtpClient.Send(mailMsg);
+        
+        }
+    }
+}
---
+++ BabyCarrotTest1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BabyCarrot.Tools;
+using BabyCarrot.Extensions;
+
+namespace BabyCarrotTest1 
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            EmailManager.Send(to, subejct, contents);
+        }
+    }
+
+
+}
